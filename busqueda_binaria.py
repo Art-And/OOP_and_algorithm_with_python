7d01bae67bb58c0464aa7658bb5d7eecbf890a5f
Andres <hir.islas@outlook.com>
2021-07-26 23:37:03 -0500

    Add changes on binary search

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -11,14 +11,16 @@ def busqueda_binaria(lista, comienzo, final, objetivo):
         return True
     elif lista[medio] < objetivo:
         return busqueda_binaria(lista, medio + 1, final, objetivo)
+    else:
+        return busqueda_binaria(lista, comienzo, medio - 1, objetivo)
 
 
 if __name__ == '__main__':
     tamano_de_lista = int(input('De que tamaño sera la lista? '))
     objetivo = int(input('Que numero quieres encontrar? '))
 
-    lista = [random.randint(0,100) for i in range(tamano_de_lista)]
-    lista = lista.sort()
+    lista = sorted([random.randint(0,100) for i in range(tamano_de_lista)])
+    #lista = lista.sort()
 
     encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
 
